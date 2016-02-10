36b98ccac64fe2f3a7e9d64c1198aee004a5473c
farooq.abdulla@smscountry.com <farooq.abdulla@smscountry.com>
2016-02-10 15:13:48 +0530

    first commit

---
+++ WebApplication1/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Optimization;
+
+namespace WebApplication1
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            ScriptBundle thirdPartyScripts = new ScriptBundle("~/Scripts/ThirdParty");
+            thirdPartyScripts.Include("~/Scripts/jquery-2.2.0.min.js",
+                                "~/Scripts/jquery-ui-1.11.4.min.js");
+
+            bundles.Add(thirdPartyScripts);
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
